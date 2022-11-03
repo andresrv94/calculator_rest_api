ccbba3b0651b8df4d6198ca38214ee65231d3c08
Andres R. Villarroel <andresrvillarroel@gmail.com>
2022-11-03 17:26:13 -0300

    Add pipeline, fix flake warnings (#1)
    * Create python-app.yml
    
    * fixed test path
    
    * Fixed Flake warnings
    
    * fixed flake warnings in test folder too
    
    * fix

--- app/app.py
+++ app/app.py
@@ -1,32 +1,42 @@
-from flask import Flask,request,render_template,make_response,redirect,url_for
+from flask import Flask, request, render_template, make_response, redirect
 import calc_functions
 
 app = Flask(__name__)
 
-#This should return a rendered HTML page explaining what can be done with the API
+
+# This should return a rendered HTML page explaining what can be done
+# with the API
 @app.route("/")
 def info():
     if request.cookies.get('throttle') == "true":
-        print("This user already downloaded this page. Redirecting him to Reddit :D")
+        print("This user already downloaded this page. \
+                Redirecting him to Reddit :D")
         return redirect('https://www.reddit.com')
 
     resp = make_response(render_template('info.html'))
     resp.set_cookie('throttle', 'true')
     return resp
 
+
 @app.route("/sum", methods=["POST"])
 def f_sum():
-    return { "result" : calc_functions.sum(float(request.form['x']), float(request.form['y'])) }
+    return {"result": calc_functions.sum(float(request.form['x']),
+            float(request.form['y']))}
+
 
 @app.route("/substract", methods=["POST"])
 def f_substract():
-    return { "result" : calc_functions.substract(float(request.form['x']), float(request.form['y'])) }
+    return {"result": calc_functions.substract(float(request.form['x']),
+            float(request.form['y']))}
+
 
 @app.route("/multiply", methods=["POST"])
 def f_multiply():
-    return { "result" : calc_functions.multiply(float(request.form['x']), float(request.form['y'])) }
+    return {"result": calc_functions.multiply(float(request.form['x']),
+            float(request.form['y']))}
+
 
 @app.route("/divide", methods=["POST"])
 def f_divide():
-    return { "result" : calc_functions.divide(float(request.form['x']), float(request.form['y'])) }
-
+    return {"result": calc_functions.divide(float(request.form['x']),
+            float(request.form['y']))}
--- app/calc_functions.py
+++ app/calc_functions.py
@@ -1,12 +1,14 @@
-def sum(x,y):
+def sum(x, y):
     return x+y
 
-def substract(x,y):
+
+def substract(x, y):
     return x-y
 
-def multiply(x,y):
+
+def multiply(x, y):
     return x*y
 
-def divide(x,y):
-    return x/y
 
+def divide(x, y):
+    return x/y
